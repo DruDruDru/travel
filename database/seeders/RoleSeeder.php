09bd70861526c49f8ad97605e1d03f0346e294dc
DruDruDru <borisrasgard251@gmail.com>
2024-10-24 14:33:46 +0700

    Add Role model

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use Carbon\Carbon;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use Illuminate\Support\Facades\DB;
+use App\Models\Role;
 
 class RoleSeeder extends Seeder
 {
@@ -14,19 +14,16 @@ class RoleSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('roles')->insert(
-            [
-                'name' => 'common',
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-            ],
-        );
-        DB::table('roles')->insert(
-            [
-                'name' => 'admin',
-                'created_at' => Carbon::now(),
-                'updated_at' => Carbon::now(),
-            ],
-        );
+        Role::create([
+            'name' => 'common',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
+
+        Role::create([
+            'name' => 'admin',
+            'created_at' => Carbon::now(),
+            'updated_at' => Carbon::now(),
+        ]);
     }
 }
